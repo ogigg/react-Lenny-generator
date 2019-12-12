56141797d6ad893275ec5f2b0585c6e2401e4e97
Oskar Gierszewski <gierszon10@gmail.com>
2019-12-13 00:47:30 +0100

    Updates

---
+++ precache-manifest.79c6625642c132a22f804d4d1a687eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b05de4bbd919a0d88b8fa9f3486979",
+    "url": "/react-Lenny-generator/index.html"
+  },
+  {
+    "revision": "fd232efa5ceb363769bc",
+    "url": "/react-Lenny-generator/static/css/main.ad337732.chunk.css"
+  },
+  {
+    "revision": "91c261d989273600161d",
+    "url": "/react-Lenny-generator/static/js/2.c7b6b84d.chunk.js"
+  },
+  {
+    "revision": "77d9a5a8d0635e146e324553e360082f",
+    "url": "/react-Lenny-generator/static/js/2.c7b6b84d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd232efa5ceb363769bc",
+    "url": "/react-Lenny-generator/static/js/main.52acf1d4.chunk.js"
+  },
+  {
+    "revision": "3ce41bdf14a89d9ec55c",
+    "url": "/react-Lenny-generator/static/js/runtime-main.efa121b6.js"
+  }
+]);
